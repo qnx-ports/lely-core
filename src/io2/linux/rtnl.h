b690007f522a54d421bc48f1443fadd6c6d8725f
Tomasz Pieczerak <tpieczerak@n7space.com>
2020-06-04 16:21:36 +0200

    CI: fix cppcheck-w64 test to use proper Docker image

--- src/io2/linux/rtnl.h
+++ src/io2/linux/rtnl.h
@@ -169,7 +169,6 @@ rtnl_recv(const struct rtnl_handle *rth, void **pbuf)
 
 		do {
 			errno = errsv;
-			// cppcheck-suppress nullPointer
 			result = recv(rth->fd, NULL, 0, MSG_PEEK | MSG_TRUNC);
 		} while (result == -1 && errno == EINTR);
 		if (result <= 0)
