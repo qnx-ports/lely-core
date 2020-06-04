fbd7afd2a9a5d5d5efe68a7bb963d3e8396666ab
Tomasz Pieczerak <tpieczerak@n7space.com>
2020-06-04 16:13:11 +0200

    co/io: review fixes

--- src/co/nmt_boot.c
+++ src/co/nmt_boot.c
@@ -1951,8 +1951,8 @@ co_nmt_boot_wait_prog_on_up_con(co_nmt_boot_t *boot, co_unsigned32_t ac,
 
 	// If the program control differs from 'Program started', try again.
 	co_unsigned8_t val = 0;
-	const uint_least8_t *begin = ptr;
-	const uint_least8_t *end = begin != NULL ? begin + n : NULL;
+	const uint_least8_t *const begin = ptr;
+	const uint_least8_t *const end = begin != NULL ? begin + n : NULL;
 	// clang-format off
 	if (!co_val_read(CO_DEFTYPE_UNSIGNED8, &val, begin, end) || val != 1)
 		// clang-format on
